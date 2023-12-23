4235fd7a017b6437cbef81cadc44f210cb73d458
Matteo Golin <matteo.golin@gmail.com>
2023-12-23 14:55:36 -0500

    Completed Day 1 Part B. Tricky with the overlapping digits; was not expecting a trick like that in the first day.

--- Makefile
+++ Makefile
@@ -1,8 +1,8 @@
 COMPLETED_DAYS = 01
-PUZZLE_INPUT = input.txt
+INPUT = input.txt
 
 DAYS = $(patsubst %,day%,$(COMPLETED_DAYS))
 SRC_DIR = src
 
 $(DAYS):
-	cargo run --bin $@ $(SRC_DIR)/$@/$(PUZZLE_INPUT)
+	cargo run --bin $@ $(SRC_DIR)/$@/$(INPUT)
--- src/day01/day01.rs
+++ src/day01/day01.rs
@@ -1,7 +1,31 @@
+use std::collections::HashMap;
 use std::env;
 use std::fs;
+use std::u32;
+
+fn get_calibration(line: &str) -> u32 {
+    let digits: Vec<u32> = line.chars().filter_map(|x| x.to_digit(10)).collect();
+    if digits.is_empty() {
+        return 0;
+    }
+    return digits.first().unwrap() * 10 + digits.last().unwrap();
+}
 
 fn main() {
+    // Compose mapping from words to numerical values
+    let digit_words: HashMap<&str, &str> = HashMap::from([
+        ("one", "o1e"),
+        ("two", "t2o"),
+        ("three", "th3ee"),
+        ("four", "f4ur"),
+        ("five", "f5ve"),
+        ("six", "s6x"),
+        ("seven", "se7en"),
+        ("eight", "ei8ht"),
+        ("nine", "n9ne"),
+    ]);
+
+    // Get command line arguments
     let args: Vec<String> = env::args().collect();
     assert!(args.len() == 2); // Only one argument: file name of input puzzle
     let filename = &args[1];
@@ -9,16 +33,17 @@ fn main() {
     // Get file contents
     let contents = fs::read_to_string(filename).expect("Could not read file.");
 
-    let mut sum = 0;
-    for line in contents.lines() {
-        let numbers: Vec<u32> = line
-            .chars()
-            .filter(|x| x.is_ascii_digit())
-            .map(|x| x.to_digit(10).expect("Non-digits should have been filtered."))
-            .collect();
-        let calibration =
-            numbers.first().expect("Expected one number.") * 10 + numbers.last().expect("Expected second number.");
-        sum += calibration;
+    // Part A
+    let sum1: u32 = contents.lines().map(get_calibration).sum();
+    println!("{}", sum1);
+
+    // Part B
+
+    // Replace digit words with real digits
+    let mut new_contents = contents;
+    for (digit, value) in digit_words.iter() {
+        new_contents = str::replace(&new_contents, digit, value);
     }
-    println!("{}", sum);
+    let sum2: u32 = new_contents.lines().map(get_calibration).sum();
+    println!("{}", sum2)
 }
