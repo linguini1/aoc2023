78c50a82cb57c9304e149207c332b128e5a890d3
Matteo Golin <matteo.golin@gmail.com>
2023-12-26 11:36:05 -0500

    Completed Day 4 Part B

--- src/day04/day04.rs
+++ src/day04/day04.rs
@@ -1,16 +1,44 @@
-use std::collections::HashSet;
+use std::collections::{HashMap, HashSet};
 use std::{env, fs};
 
 struct Card {
-    nums: HashSet<u32>,
+    id: u32,
+    winning_nums: HashSet<u32>,
+    user_nums: HashSet<u32>,
 }
 
 impl Card {
     fn from_line(line: &str) -> Card {
         let binding = line.split(':').collect::<Vec<_>>();
+
+        // Card ID
         let card = binding.first().expect("No card identifier!");
+        let mut id = card.replace("Card", "");
+        id.retain(|c| !c.is_whitespace());
+
+        // Numbers
+        let numbers = binding.get(1).expect("No numbers!");
+        let number_sets = numbers.split('|').collect::<Vec<_>>();
+        let winners = number_sets.first().expect("Expected winning numbers");
+        let users = number_sets.get(1).expect("Expected user numbers");
+
         Card {
-            nums: get_numbers(binding.get(1).expect("No winning numbers!")),
+            id: id.parse().expect("Expected card ID."),
+            winning_nums: get_numbers(winners),
+            user_nums: get_numbers(users),
+        }
+    }
+
+    fn next_n_cards(&self) -> u32 {
+        self.winning_nums.intersection(&self.user_nums).count() as u32
+    }
+
+    fn points(&self) -> u32 {
+        let winning_count = self.next_n_cards();
+        if winning_count > 0 {
+            2_u32.pow(winning_count - 1)
+        } else {
+            0
         }
     }
 }
@@ -28,21 +56,30 @@ fn main() {
 
     let contents = fs::read_to_string(filename).expect("Could not read file.");
 
-    let points: u32 = contents
-        .lines()
-        .map(|l| {
-            let binding = l.split('|').collect::<Vec<_>>();
-            let card_contents = binding.first().expect("Expected winning numbers.");
-            let nums = get_numbers(binding.get(1).expect("Expected user numbers."));
-            let card = Card::from_line(card_contents);
+    // Get cards
+    let cards: Vec<_> = contents.lines().map(Card::from_line).collect();
 
-            let winners = card.nums.intersection(&nums).count();
-            if winners > 0 {
-                2_u32.pow(winners as u32 - 1)
-            } else {
-                0
-            }
-        })
-        .sum();
+    // Part A
+    let points: u32 = cards.iter().map(|c| c.points()).sum();
     println!("{points}");
+
+    // Part B
+    let mut card_counts: HashMap<u32, u32> = HashMap::new();
+
+    // Always one card initially
+    for card in &cards {
+        card_counts.insert(card.id, 1);
+    }
+
+    // Go through all cards
+    for card in &cards {
+        // Go through all card copies
+        for copy in card.id + 1..card.id + card.next_n_cards() + 1 {
+            let parent_card_count = *card_counts.get(&card.id).unwrap();
+            card_counts.entry(copy).and_modify(|count| *count += parent_card_count);
+        }
+    }
+
+    let total_cards: u32 = card_counts.values().sum();
+    println!("{total_cards}");
 }
