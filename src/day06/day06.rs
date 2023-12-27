8c4be8b401b6f700411ac3ad95bbbf120b942d67
Matteo Golin <matteo.golin@gmail.com>
2023-12-27 12:17:47 -0500

    Completed Day 6 Part B.

--- src/day06/day06.rs
+++ src/day06/day06.rs
@@ -2,13 +2,13 @@ use core::iter::zip;
 use std::env;
 use std::fs;
 
-fn parse_numbers(text: &str) -> Vec<u32> {
+fn parse_numbers(text: &str) -> Vec<u64> {
     text.split_whitespace()
         .map(|n| n.parse().expect("Expected number."))
         .collect()
 }
 
-fn distance(hold_time: u32, total_time: u32) -> u32 {
+fn distance(hold_time: u64, total_time: u64) -> u64 {
     hold_time * (total_time - hold_time)
 }
 
@@ -20,12 +20,29 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Could not read file.");
     let mut lines = contents.lines();
 
-    let times = parse_numbers(&lines.next().expect("Expected times.").replace("Time: ", ""));
-    let distances = parse_numbers(&lines.next().expect("Expected distances.").replace("Distance: ", ""));
+    let raw_times = &lines.next().expect("Expected times.").replace("Time: ", "");
+    let raw_distances = &lines.next().expect("Expected distances.").replace("Distance: ", "");
+
+    // Part A
+    let times = parse_numbers(raw_times);
+    let distances = parse_numbers(raw_distances);
 
     let winning_options = zip(&times, &distances)
-        .map(|(t, d)| (0..*t).filter(|time| distance(*time, *t) > *d).count() as u32)
+        .map(|(t, d)| (0..*t).filter(|time| distance(*time, *t) > *d).count() as u64)
         .reduce(|acc, e| acc * e)
         .unwrap();
+
     println!("{winning_options}");
+
+    // Part B
+    let mut time_str = raw_times.clone();
+    time_str.retain(|c| c.is_ascii_digit());
+    let time: u64 = time_str.parse().unwrap();
+
+    let mut distance_str = raw_distances.clone();
+    distance_str.retain(|c| c.is_ascii_digit());
+    let dist: u64 = distance_str.parse().unwrap();
+
+    let option_count = (0..time).filter(|t| distance(*t, time) > dist).count() as u64;
+    println!("{option_count}");
 }
