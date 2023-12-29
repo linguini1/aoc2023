c7a800092b5cbe4addc4f21fe1a418cd51d014dc
Matteo Golin <matteo.golin@gmail.com>
2023-12-29 17:46:08 -0500

    Had to check what other people were using to solve this because traversing the nodes was taking too long. Least common multiple seemed to be the answer, to see when the cycles would collide. I would not have figured this out because it required noticing that the distance from a-z is a prime number and a cyclic graph.

--- src/day08/day08.rs
+++ src/day08/day08.rs
@@ -15,6 +15,38 @@ fn parse_node(line: &str) -> (&str, (&str, &str)) {
     (name.as_str(), (left.as_str(), right.as_str()))
 }
 
+fn next_step<'a>(node: &str, direction: Option<char>, graph: &'a HashMap<&'a str, (&'a str, &'a str)>) -> &'a str {
+    let nodes = graph.get(node).unwrap();
+    match direction {
+        Some('R') => nodes.1,
+        Some('L') => nodes.0,
+        _ => panic!("Expected Right or Left direction."),
+    }
+}
+
+fn steps_until_destination(
+    start: &str,
+    directions: &str,
+    graph: &HashMap<&str, (&str, &str)>,
+    condition: fn(&str) -> bool,
+) -> u64 {
+    let mut steps: u64 = 0;
+    let mut direction_cycle = directions.chars().cycle();
+    let mut node = start;
+    while !condition(node) {
+        node = next_step(node, direction_cycle.next(), graph);
+        steps += 1;
+    }
+    steps
+}
+
+fn gcd(mut x: u64, mut y: u64) -> u64 {
+    while y != 0 {
+        (x, y) = (y, x % y)
+    }
+    x
+}
+
 fn main() {
     let args: Vec<_> = env::args().collect();
     assert!(args.len() == 2);
@@ -23,7 +55,7 @@ fn main() {
     let contents = fs::read_to_string(filename).expect("Could not open file.");
 
     let mut lines = contents.lines();
-    let mut directions = lines.next().expect("Expected directions.").chars().cycle();
+    let directions = lines.next().expect("Expected directions.");
     lines.next().expect("Expected blank line.");
 
     let mut graph = HashMap::new();
@@ -32,16 +64,22 @@ fn main() {
         graph.insert(node, nodes);
     }
 
-    let mut node = START_NODE;
-    let mut steps = 0;
-    while node != END_NODE {
-        let nodes = graph.get(node).unwrap();
-        match directions.next() {
-            Some('R') => node = nodes.1,
-            Some('L') => node = nodes.0,
-            _ => panic!("Expected Right or Left direction."),
-        }
-        steps += 1;
-    }
+    // Part A
+    let steps = steps_until_destination(START_NODE, directions, &graph, |n: &str| n == END_NODE);
     println!("{steps}");
+
+    // Part B
+    let start_nodes = Vec::from_iter(graph.keys().filter(|k| k.ends_with('A')).copied());
+    let is_end_node = |n: &str| n.ends_with('Z'); // Test if a node is destination
+    let multi_steps: Vec<_> = start_nodes
+        .iter()
+        .map(|n| steps_until_destination(n, directions, &graph, is_end_node))
+        .collect();
+
+    let mut cycles_collide = 1;
+    for step in multi_steps {
+        cycles_collide = cycles_collide * step / gcd(cycles_collide, step);
+    }
+
+    println!("{cycles_collide}");
 }
