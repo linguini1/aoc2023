da7c5a956501d6464f8898283711f9d17c16bc76
Matteo Golin <matteo.golin@gmail.com>
2024-01-01 15:00:16 -0500

    Completed Day 9 Part A.

---
+++ src/day09/day09.rs
@@ -0,0 +1,36 @@
+use std::env;
+use std::fs;
+
+#[derive(Debug)]
+struct Readings {
+    raw: Vec<i64>,
+}
+
+impl Readings {
+    fn next_val(&self) -> i64 {
+        let mut finals: Vec<i64> = vec![];
+        let mut working_set: Vec<i64> = self.raw.to_vec();
+        while !working_set.iter().all(|n| *n == 0) {
+            finals.push(*working_set.last().expect("Expected at least one digit."));
+            working_set = working_set.windows(2).map(|w| w[1] - w[0]).collect();
+        }
+        finals.iter().sum()
+    }
+}
+
+fn main() {
+    let args: Vec<String> = env::args().collect();
+    assert!(args.len() == 2);
+    let filename = args.get(1).expect("Expected filename.");
+
+    let contents = fs::read_to_string(filename).expect("Could not read file.");
+
+    let readings: Vec<Readings> = contents
+        .lines()
+        .map(|l| Readings {
+            raw: l.split(' ').map(|n| n.parse().expect("Expected number.")).collect(),
+        })
+        .collect();
+    let next_value_sum: i64 = readings.iter().map(|r| r.next_val()).sum();
+    println!("{next_value_sum}");
+}
